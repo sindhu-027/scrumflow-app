c7452a944efc0cc17220019e33059c1805dd52a1
sindhu-027 <ssindhu2502@gmail.com>
2026-04-16 20:00:54 +0530

    final commit

--- SprintManagementAPI/Program.cs
+++ SprintManagementAPI/Program.cs
@@ -40,6 +40,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // ===================== MIDDLEWARE =====================
 app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
