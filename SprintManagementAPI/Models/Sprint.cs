642bec5686ff01a7b7af571b3574e3a423580cb9
sindhu-027 <ssindhu2502@gmail.com>
2026-04-20 18:02:25 +0530

    connecting backend with frontend URL update

--- SprintManagementAPI/Models/Sprint.cs
+++ SprintManagementAPI/Models/Sprint.cs
@@ -1,37 +1,3 @@
-// using System;
-// using System.Collections.Generic;
-// using System.ComponentModel.DataAnnotations;
-
-// namespace SprintManagementAPI.Models
-// {
-//     public class Sprint
-//     {
-//         public int Id { get; set; }
-
-//         [Required]
-//         [MaxLength(150)]
-//         public string Name { get; set; } = "";
-
-//         [Required]
-//         public DateTime StartDate { get; set; }
-
-//         [Required]
-//         public DateTime EndDate { get; set; }
-
-//         [Required]
-//         public string Description { get; set; } = "";
-
-//         // ✅ NEW FIELD (IMPORTANT)
-//         [Required]
-//         public string Status { get; set; } = "Planned"; 
-//         // Planned | Active | Completed
-
-//         // Optional navigation property
-//         public List<TaskModel>? Tasks { get; set; }
-//     }
-// }
-
-//------------------------------
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
