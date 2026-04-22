677eb96538d1a42199c677bee405e955d1fb318d
sindhu-027 <ssindhu2502@gmail.com>
2026-04-22 10:14:02 +0530

    fix schema

---
+++ SprintManagementAPI/Migrations/20260422044214_FixSprintSchema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SprintManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSprintSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
